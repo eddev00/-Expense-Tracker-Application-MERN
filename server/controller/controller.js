7937917aecb5b264913c4be2b23a4993b595c130
unknown <eddevjo@gmail.com>
2023-02-21 11:20:19 +0100

    hiding env files

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,12 @@
-//get categories
+const model = require("../models/model.js");
 
-function create_Cat(req, res) {
-  res.json("Get Request from Categories");
-}
+//get categories
+function create_Cat(req, res) {}
+const Create = new model.Categories({
+  type: "Savings",
+  color: "1F3B5C",
+});
+//
 
 module.exports = {
   create_Cat,
